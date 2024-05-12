074bc0e943eab117aa0aada885aa9e720b75760e
bobr <bobrus91@gmail.com>
2024-05-12 13:39:33 +0300

    Subscription updating migrated to camunda

--- mainMicroservice/src/main/java/bobr/mainMicroservice/subscription/SubscriptionAutoUpdater.java
+++ mainMicroservice/src/main/java/bobr/mainMicroservice/subscription/SubscriptionAutoUpdater.java
@@ -1,21 +1,34 @@
 package bobr.mainMicroservice.subscription;
 
+import bobr.mainMicroservice.camunda.ExternalTaskClientService;
 import bobr.mainMicroservice.user.UserService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskService;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SubscriptionAutoUpdater {
 
     private final UserService userService;
+    private final ExternalTaskClientService externalTaskClientService;
 
-    @Scheduled(cron = "${SUBSCRIPTION_RENEW_PERIOD}")
-    public void renew() {
+    @PostConstruct
+    public void addCamundaTaskHandler() {
+        externalTaskClientService.addHandlerToTopic("sub-renewing", this::handleExternalTask);
+    }
+
+    private void handleExternalTask(ExternalTask externalTask, ExternalTaskService externalTaskService) {
         userService
                 .findUsersWithLessThanOneDaySubscription()
                 .forEach(userService::buySubscription);
+
+        externalTaskService.complete(externalTask);
+        log.info("Subscriptions updated");
     }
 
 }
